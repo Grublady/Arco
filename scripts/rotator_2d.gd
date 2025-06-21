dacb4944efbe6930f00b30c5cf5cc3141e6713a8
Serena <serena@serenamaar.com>
2025-06-21 14:10:13 -0400

    Simplify rotation offset via global_rotation

--- scripts/rotator_2d.gd
+++ scripts/rotator_2d.gd
@@ -1,14 +1,7 @@
 class_name Rotator2D extends Node2D
 
-@export var rotating := true:
-	set(new_value):
-		if new_value == rotating:
-			return
-		rotating = new_value
-		if rotating:
-			offset = -RotationInput.rotation
-@export var offset: float = 0
+@export var rotating := true
 
 func _process(_delta: float) -> void:
 	if rotating:
-		rotation = RotationInput.rotation + offset
+		global_rotation = RotationInput.rotation
