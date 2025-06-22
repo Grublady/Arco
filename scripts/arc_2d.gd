8ac5e95643c9082ae2697073bb5d2f5d7e13072b
Serena <serena@serenamaar.com>
2025-06-22 15:28:19 -0400

    Factor out Arc2D

--- scripts/arc_2d.gd
+++ scripts/arc_2d.gd
@@ -1,5 +1,5 @@
 @tool
-extends Rotator2D
+class_name Arc2D extends Node2D
 
 @export var radius: float = 50:
 	set(new_value):
@@ -13,11 +13,17 @@ extends Rotator2D
 			return
 		width = new_value
 		queue_redraw()
-@export_range(0, 0.5, 0.01) var gap: float = 0.1:
+@export_range(0, 1, 0.01) var length: float = 0.1:
 	set(new_value):
-		if new_value == gap:
+		if new_value == length:
 			return
-		gap = new_value
+		length = new_value
+		queue_redraw()
+@export_range(-1, 1, 0.01) var offset: float = -0.25:
+	set(new_value):
+		if new_value == offset:
+			return
+		offset = new_value
 		queue_redraw()
 @export var color: Color = Color.WHITE:
 	set(new_value):
@@ -26,11 +32,7 @@ extends Rotator2D
 		color = new_value
 		queue_redraw()
 
-func _process(delta: float) -> void:
-	if not Engine.is_editor_hint():
-		super._process(delta)
-
 func _draw() -> void:
-	var start_angle := -PI/2 + gap * TAU
-	var end_angle := 3*PI/2 - gap * TAU
+	var start_angle := offset * TAU - length * PI
+	var end_angle := offset * TAU + length * PI
 	draw_arc(Vector2.ZERO, radius, start_angle, end_angle, 64, color, width, true)
--- scripts/ringleader.gd
+++ scripts/ringleader.gd
@@ -3,9 +3,16 @@ extends Node2D
 
 @onready var ring1 := $Ring1
 @onready var ring2 := $Ring1/Ring2
+var indicator_scene: PackedScene = preload("res://scenes/indicator.tscn")
+
+var _indicators: Array[Node] = []
 
 func _ready() -> void:
 	EventBus.audio_event.connect(_on_audio_event)
+
+func _process(_delta: float) -> void:
+	for indicator in _indicators:
+		indicator.radius -= 1
 
 func _on_audio_event(data: Array[String]) -> void:
 	match data[0]:
@@ -15,6 +22,10 @@ func _on_audio_event(data: Array[String]) -> void:
 		"ring2":
 			ring1.rotating = false
 			ring2.rotating = true
+		"indicator":
+			var new_indicator := indicator_scene.instantiate()
+			add_child(new_indicator)
+			_indicators.append(new_indicator)
 		"print":
 			print(data[1])
 		_:
