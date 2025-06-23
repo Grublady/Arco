1cca026ee69c6d2ee255b02a8eb5bf3c2f768276
Serena <serena@serenamaar.com>
2025-06-23 12:25:50 -0400

    Improve audio event response structure, add "pin"

--- scripts/ringleader.gd
+++ scripts/ringleader.gd
@@ -15,7 +15,29 @@ func _ready() -> void:
 	EventBus.audio_event.connect(_on_audio_event)
 
 func _on_audio_event(event: EventBus.AudioEvent) -> void:
-	match event.data[0]:
+	match event.data.pop_front():
+		"activate":
+			activate_ring(event.data[0])
+		"indicator", "pin":
+			create_indicator(event.data[0], float(event.data[1]), event.duration)
+		"end":
+			match event.data.pop_front():
+				"indicator":
+					check(event.data[0], float(event.data[1]))
+				"pin":
+					check(event.data[0], float(event.data[1]))
+					match event.data.pop_front():
+						"ring1":
+							activate_ring("ring2")
+						"ring2":
+							activate_ring("ring1")
+		"print":
+			print(event.data[0])
+		_:
+			return
+
+func activate_ring(ring_name: String) -> void:
+	match ring_name:
 		"ring1":
 			ring1.rotating = true
 			ring1_arc.modulate = Color.WHITE
@@ -26,38 +48,35 @@ func _on_audio_event(event: EventBus.AudioEvent) -> void:
 			ring1_arc.modulate = Color(1, 1, 1, 0.5)
 			ring2.rotating = true
 			ring2_arc.modulate = Color.WHITE
-		"indicator":
-			var new_indicator := indicator_scene.instantiate()
-			new_indicator.radius = 300
-			match event.data[1]:
-				"ring1":
-					new_indicator.target_radius = ring1_arc.radius
-					new_indicator.color = ring1_indicator_color
-				"ring2":
-					new_indicator.target_radius = ring2_arc.radius
-					new_indicator.color = ring2_indicator_color
-			new_indicator.rotation = TAU * float(event.data[2])
-			new_indicator.tween_time = event.duration
-			add_child(new_indicator)
-		"end":
-			var current_rotation: float = 0
-			var location_radius: float = 0
-			match event.data[2]:
-				"ring1":
-					current_rotation = ring1.global_rotation
-					location_radius = ring1_arc.radius
-				"ring2":
-					current_rotation = ring2.global_rotation
-					location_radius = ring2_arc.radius
-			
-			var target_rotation := float(event.data[3]) * TAU
-			var difference := angle_difference(target_rotation, current_rotation)
-			var success := (absf(difference) <= PI * check_hit_range)
-			
-			var event_position := to_global(position + Vector2.UP.rotated(target_rotation) * location_radius)
-			
-			EventBus.check_event.emit(success, event_position, current_rotation)
-		"print":
-			print(event.data[1])
-		_:
-			return
+
+func create_indicator(ring_name: String, event_rotation: float, event_duration: float) -> void:
+	var new_indicator := indicator_scene.instantiate()
+	new_indicator.radius = 300
+	match ring_name:
+		"ring1":
+			new_indicator.target_radius = ring1_arc.radius
+			new_indicator.color = ring1_indicator_color
+		"ring2":
+			new_indicator.target_radius = ring2_arc.radius
+			new_indicator.color = ring2_indicator_color
+	new_indicator.rotation = TAU * event_rotation
+	new_indicator.tween_time = event_duration
+	add_child(new_indicator)
+
+func check(ring_name: String, target_rotation: float) -> void:
+	var current_rotation: float
+	var location_radius: float
+	match ring_name:
+		"ring1":
+			current_rotation = ring1.global_rotation
+			location_radius = ring1_arc.radius
+		"ring2":
+			current_rotation = ring2.global_rotation
+			location_radius = ring2_arc.radius
+	
+	var difference := angle_difference(target_rotation * TAU, current_rotation)
+	var success := (absf(difference) <= PI * check_hit_range)
+	
+	var event_position := to_global(position + Vector2.UP.rotated(target_rotation) * location_radius)
+	
+	EventBus.check_event.emit(success, event_position, current_rotation)
