36650693bcb6c1566aad7b880de7a2f9f62d9fb1
Serena <serena@serenamaar.com>
2025-06-26 15:14:55 -0400

    Refactor main menu script to improve sanity

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,13 +2,44 @@ extends Control
 
 @onready var selection_rotator := $Confirmation/Rotator2D
 
+func finish() -> void:
+	get_tree().change_scene_to_file("res://scenes/songs/xogot_jam_jam.tscn")
+
+func show_input_modes() -> void:
+	$InputModeSelection.show()
+func hide_input_modes() -> void:
+	$InputModeSelection.hide()
+
+func show_input_confirmation() -> void:
+	$Confirmation.show()
+	$Confirmation.process_mode = Node.PROCESS_MODE_INHERIT
+	selection_rotator.rotating = true
+func hide_input_confirmation() -> void:
+	$Confirmation.hide()
+	$Confirmation.process_mode = Node.PROCESS_MODE_DISABLED
+	selection_rotator.rotating = false
+	selection_rotator.rotation = 0
+
+func show_latency_check() -> void:
+	$LatencyCheck.start()
+func hide_latency_check() -> void:
+	$LatencyCheck.end()
+
 func _ready() -> void:
 	for button in $InputModeSelection/ButtonContainer.get_children():
-		button.pressed.connect(_on_input_mode_button_pressed)
-	$Confirmation/ConfirmOption.pressed.connect(_on_input_confirm_pressed)
-	$Confirmation/CancelButton.pressed.connect(_on_input_cancel_pressed)
-	$LatencyCheck/ConfirmButton.pressed.connect(_on_latency_confirm_pressed)
-	$LatencyCheck/CancelButton.pressed.connect(_on_latency_cancel_pressed)
+		button.pressed.connect(hide_input_modes)
+		button.pressed.connect(show_input_confirmation)
+	
+	$Confirmation/ConfirmOption.pressed.connect(hide_input_confirmation)
+	$Confirmation/CancelButton.pressed.connect(hide_input_confirmation)
+	$Confirmation/ConfirmOption.pressed.connect(show_latency_check)
+	$Confirmation/CancelButton.pressed.connect(show_input_modes)
+	
+	$LatencyCheck/CancelButton.pressed.connect(hide_latency_check)
+	$LatencyCheck/CancelButton.pressed.connect(show_input_confirmation)
+	
+	$LatencyCheck/ConfirmButton.pressed.connect(finish)
+	
 	$Confirmation.process_mode = Node.PROCESS_MODE_DISABLED
 
 func _process(_delta: float) -> void:
@@ -20,33 +51,3 @@ func _process(_delta: float) -> void:
 		$Confirmation/ConfirmOption.active = false
 		$Confirmation/PressTarget.hide()
 		$Confirmation/TutorialArrow.show()
-
-func _on_input_mode_button_pressed() -> void:
-	$InputModeSelection.hide()
-	$InputModeSelection.process_mode = Node.PROCESS_MODE_DISABLED
-	$Confirmation.show()
-	$Confirmation.process_mode = Node.PROCESS_MODE_INHERIT
-	selection_rotator.rotating = true
-
-func _on_input_cancel_pressed() -> void:
-	$InputModeSelection.show()
-	$InputModeSelection.process_mode = Node.PROCESS_MODE_INHERIT
-	$Confirmation.hide()
-	$Confirmation.process_mode = Node.PROCESS_MODE_DISABLED
-	selection_rotator.rotating = false
-	selection_rotator.rotation = 0
-
-func _on_input_confirm_pressed() -> void:
-	$Confirmation.hide()
-	$Confirmation.process_mode = Node.PROCESS_MODE_DISABLED
-	selection_rotator.rotating = false
-	selection_rotator.rotation = 0
-	$LatencyCheck.start()
-
-func _on_latency_cancel_pressed() -> void:
-	$LatencyCheck.end()
-	$InputModeSelection.show()
-	$InputModeSelection.process_mode = Node.PROCESS_MODE_INHERIT
-
-func _on_latency_confirm_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/songs/xogot_jam_jam.tscn")
