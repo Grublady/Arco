ff34c8218517b4723aa0f3018627295c4acdb24a
Serena <serena@serenamaar.com>
2025-06-28 22:57:45 -0400

    Automatically exit song when finished

--- scripts/song.gd
+++ scripts/song.gd
@@ -2,3 +2,4 @@ extends Node2D
 
 func _ready() -> void:
 	$EventfulAudioStreamPlayer.custom_latency = Settings.latency
+	$EventfulAudioStreamPlayer.finished.connect(get_tree().change_scene_to_file.bind("res://scenes/menu.tscn"))
